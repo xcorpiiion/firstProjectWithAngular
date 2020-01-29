96b810cf6603b87074879d96553d382362e516d9
Vinicius Cruz <vinicius.das.silva.cruz.sanji@gmail.com>
2020-01-29 16:56:21 -0300

    bugs no projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatDialog} from '@angular/material';
 
+export class PizzaPartyAppModule { }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fisrtProject';
+
+  constructor(public dialog: MatDialog) {
+
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(AppComponent, {
+      height: '350px'
+    });
+    console.assert("aaa");
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
